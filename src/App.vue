f0c0f8e6d4ab50bb82dac631fae23edb56eef80a
HuangJM7 <jm_william@qq.com>
2019-11-24 09:37:53 +0800

    vue 井字棋

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,94 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>第{{n}}手</div>
+    <div class="chest">
+      <div class="row">
+        <Cell @click="onClickCell(0,$event)" :n="n" />
+        <Cell @click="onClickCell(1,$event)" :n="n" />
+        <Cell @click="onClickCell(2,$event)" :n="n" />
+      </div>
+      <div class="row">
+        <Cell @click="onClickCell(3,$event)" :n="n" />
+        <Cell @click="onClickCell(4,$event)" :n="n" />
+        <Cell @click="onClickCell(5,$event)" :n="n" />
+      </div>
+      <div class="row">
+        <Cell @click="onClickCell(6,$event)" :n="n" />
+        <Cell @click="onClickCell(7,$event)" :n="n" />
+        <Cell @click="onClickCell(8,$event)" :n="n" />
+      </div>
+    </div>
+    <div>{{ result===false ? "胜负未分" : `${result}胜`}}</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cell from "./Cell.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Cell
+  },
+  data() {
+    return {
+      n: 0,
+      result: false,
+      map: [[null, null, null], [null, null, null], [null, null, null]]
+    };
+  },
+  methods: {
+    onClickCell(i, text) {
+      this.map[Math.floor(i / 3)][i % 3] = text;
+      this.n = this.n + 1;
+      this.tell();
+    },
+    tell() {
+      const map = this.map;
+      for (let i = 0; i < 2; i++) {
+        if (
+          map[i][0] !== null &&
+          map[i][0] === map[i][1] &&
+          map[i][2] === map[i][1]
+        ) {
+          this.result = map[i][0];
+        }
+      }
+      for (let j = 0; j < 2; j++) {
+        if (
+          map[0][j] !== null &&
+          map[0][j] === map[1][j] &&
+          map[1][j] === map[2][j]
+        ) {
+          this.result = map[0][j];
+        }
+      }
+      if (
+        map[0][0] !== null &&
+        map[0][0] === map[1][1] &&
+        map[2][2] === map[1][1]
+      ) {
+        this.result = map[0][0];
+      }
+      if (
+        map[0][2] !== null &&
+        map[0][2] === map[1][1] &&
+        map[2][0] === map[1][1]
+      ) {
+        this.result = map[0][2];
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: grid;
+  justify-content: center;
+  justify-items: center;
+}
+.row {
+  display: flex;
 }
 </style>
